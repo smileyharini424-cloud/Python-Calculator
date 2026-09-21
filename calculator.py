90aaf98a3b471d3e84d773a5372f3f4a0837e5fb
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-20 22:35:33 -0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
